944e2d62bdb0ef9677e02a875af8a08f265f9d6e
sashpawar11 <saisaipawar@gmail.com>
2022-05-10 15:49:13 +0530

    eventnamechanges

--- login.php
+++ login.php
@@ -1,27 +1,29 @@
 <?php
 
-$dbcon=mysqli_connect("localhost","procyon2022","");
-mysqli_select_db($dbcon,"procyon2022");
+$con = mysqli_connect('localhost', 'root', '', 'procyon2022') or die("Could not connect to mysql".mysqli_error($con));
 
-/*if($dbcon==1)
-{
-    echo" connected";
-}
-else
-{
-    echo "not connected";
-}*/
+
+// if($con)
+// {
+//     echo" connected";
+// }
+// else
+// {
+//     echo "not connected";
+// }
 
 if(isset($_POST['login']))
 {
     $user_name=$_POST['username'];
+    // echo $user_name;
     $user_pass=$_POST['password'];
     $category=$_POST['event-category'];
 
     if($category=='class-event')
     {
-        $check_user1="select * from cregistrations WHERE username='".$user_name."'AND password='".$user_pass."'";
-        $run1=mysqli_query($dbcon,$check_user1);
+        $check_user1="select * from crregistration WHERE username='$user_name'AND password='$user_pass' ";
+        $run1=mysqli_query($con,$check_user1);
+        $num1 = mysqli_num_rows($run1);
         if(mysqli_num_rows($run1)==1)
         {
           echo "<script>window.open('https://docs.google.com/spreadsheets/d/1NqXqwXTfuSulkDSO88Vaau9jeZX7AWr98rQdpKkzTl8/edit?usp=sharing','_self')</script>";
@@ -32,8 +34,8 @@ if(isset($_POST['login']))
     }
     else if($category=='department-event')
     {
-        $check_user2="select * from deptregistrations WHERE username='".$user_name."'AND password='".$user_pass."'";
-        $run2=mysqli_query($dbcon,$check_user2);
+        $query="select * from deptregistrations WHERE username='".$user_name."'AND password='".$user_pass."'";
+        $run2=mysqli_query($con,$query);
         if(mysqli_num_rows($run2)==1)
         {
           echo "<script>window.open('https://drive.google.com/file/d/10yT9BA_4uVOOuzh8OtcxWIwqz1uO4hnn/view?usp=sharing','_self')</script>";
